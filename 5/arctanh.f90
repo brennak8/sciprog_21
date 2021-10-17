e40e3ebce9b08b66ebc35d74ebf0a8b6c1d80fc1
brennak8 <brennak8@tcd.ie>
2021-10-17 15:37:50 +0100

    Functions work as intended, need to make storage in arrays faster

--- 5/arctanh.f90
+++ 5/arctanh.f90
@@ -1,58 +1,54 @@
 program arctanh
   implicit none
-  real (kind=8), dimension(181) :: arctanh1_x, arctanh2_x
-  real (kind=8) :: res1, res2, delta, x
-  integer (kind=4) :: j, i
+  real (kind=4), dimension(182) :: arctanh1_x, arctanh2_x
+  real (kind=4) :: res1, res2, delta, x
+  integer (kind=4) :: j, i, fileunit
  
   x = -0.9
   print *, 'Please provide a precision'
   print *, 'Must be a real positive number'
   read (5,*) delta
-  
-!  do i = 1,182,1
-!    x = x + (real(i)/100.)
-!    res1 = artanh1(x,delta)
-!    print *, res1
-!    res2 = artanh2(x)
-!    print *, res2
-!    arctanh1_x(i) = res1
-!    arctanh2_x(i) = res2
-!  end do
 
-  print *, artanh1(x,delta)
-  x = x + 0.01
-  print *, artanh1(x,delta)
+
+  do i = 0,181,1
+    x = x + (real(i)/100.)
+    arctanh1_x(i+1) = artanh1(x,delta)
+    arctanh2_x(i+1) = artanh2(x)
+  end do
   
+  print *, arctanh1_x(4)
+  print *, arctanh2_x(4)
  
 contains
-  real function artanh1(x, delta)
+
+  real function artanh1(x,delta)
     !Uses MacLaurin series to express hyperbolic arc tangent
-    real (kind=8), intent(in) :: x, delta
-    real (kind=8) :: y
-    real (kind=8) :: n = 0.
-    integer (kind=4) :: z
+    real (kind=4), intent(in) :: x
+    real (kind=4) :: y, y_1, delta, n
+    integer :: z
 
+    n = 0.
+    y = 0.
+    y_1 = 0.
     z = 1
-
     do while (z.eq.1)
-      y = y + (x**(2.*n + 1.))/(2.*n + 1.)
-      print *, y, 'y', x
+      y = y + (x**(2.*n + 1))/(2.*n + 1)
+      y_1 = (x**(2.*n + 1))/(2.*n + 1.)
       n = n + 1.
-      print *, n, 'n', x
-      if (y.lt.delta) then
+      if (abs(y_1).lt.delta) then
         z = 0
       end if
     end do
-
-    artanh1 = y
+   
+    artanh1 =  y
 
   end function artanh1
 
   real function artanh2(x)
     !Uses natural logarithms to express hyperbolic arc tangent
-    real (kind=8), intent(in) :: x
+    real (kind=4), intent(in) :: x
 
-      artanh2 = 0.5*(log(1. + x) - log(1. - x))
+      artanh2 = 0.5*(log((1. + x)/(1. -x)))
 
   end function artanh2
 
